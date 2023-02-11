a82269edd83554936e5967a1e171aa8e0320c5c4
Tomjosetj31 <tomjosethomastj31@gmail.com>
2023-02-12 00:45:11 +0530

    add book now page

--- src/Components/BookNow.jsx
+++ src/Components/BookNow.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import { Button } from "@mui/material";
+import React, { useState } from "react";
+import DateTimePicker from "react-datetime-picker";
 
 const BookNow = () => {
+  const [value, onChange] = useState(new Date());
+
   return (
     <div>
-      This is the bookings page !!!
+      <DateTimePicker onChange={onChange} value={value} />
+      <Button
+        style={{ backgroundColor: "blue" }}
+        onClick={() => {
+          console.log(value);
+        }}
+      >
+        Create Booking
+      </Button>
     </div>
-  )
-}
+  );
+};
 
-export default BookNow
+export default BookNow;
