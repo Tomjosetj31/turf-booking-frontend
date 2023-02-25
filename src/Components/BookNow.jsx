bc036c3f2d4be2289f2b12902960c3db335c89e0
Tomjosetj31 <tomjosethomastj31@gmail.com>
2023-02-26 00:29:56 +0530

    add booking calendar

--- src/Components/BookNow.jsx
+++ src/Components/BookNow.jsx
@@ -1,19 +1,49 @@
 import { Button } from "@mui/material";
 import React, { useState } from "react";
 import DateTimePicker from "react-datetime-picker";
+import TimePicker from "react-time-picker";
+import axios from "axios";
 
 const BookNow = () => {
-  const [value, onChange] = useState(new Date());
+  let token = localStorage.getItem("token");
+  const [startTime, setStartTime] = useState(new Date());
+  const [endTime, setEndTime] = useState(new Date());
+  const data = {
+    bookingDate: startTime,
+    start_time: startTime,
+    end_time: endTime,
+  };
 
+  const options = {
+    headers: {
+      "Content-Type": "application/json",
+      authorization: `Bearer ${token}`,
+    },
+  };
+  const sendRequest = async () => {
+    const res = await axios
+      .post(`http://localhost:5000/add`, data, options)
+      .catch((err) => console.log(err));
+
+    const response = await res.data;
+    console.log(response);
+    return response;
+  };
   return (
     <div>
-      <DateTimePicker onChange={onChange} value={value} />
-      <Button
-        style={{ backgroundColor: "blue" }}
-        onClick={() => {
-          console.log(value);
+      <DateTimePicker
+        onChange={(event) => {
+          setStartTime(event.target.value);
         }}
-      >
+        value={startTime}
+      />
+      <TimePicker
+        onChange={(event) => {
+          setEndTime(event.target.value);
+        }}
+        value={endTime}
+      />
+      <Button style={{ backgroundColor: "blue" }} onClick={() => sendRequest()}>
         Create Booking
       </Button>
     </div>
